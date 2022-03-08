e7e35e94c153c3b8a3be8bdcae4aa51353c56c5c
Grider <himekifee@gmail.com>
2022-03-08 01:12:26 +0000

    Fix product name

--- lineage_gts7xl.mk
+++ lineage_gts7xl.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/samsung/gts7xl/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_gts7xlxeea
+PRODUCT_NAME := lineage_gts7xl
 PRODUCT_DEVICE := gts7xl
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
